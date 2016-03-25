c716f92430c956601a2dd7c35607ac671fdf3d12
Ben Norris <ben@bsn.io>
2016-03-25 10:02:10 -0700

    Set up blank framework

---
+++ Sources/VersionNumber.h
@@ -0,0 +1,19 @@
+//
+//  VersionNumber.h
+//  VersionNumber
+//
+//  Created by Ben Norris on 3/25/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VersionNumber.
+FOUNDATION_EXPORT double VersionNumberVersionNumber;
+
+//! Project version string for VersionNumber.
+FOUNDATION_EXPORT const unsigned char VersionNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VersionNumber/PublicHeader.h>
+
+
