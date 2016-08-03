d1c330b79252e7a43ea3517f89a09c343d7353db
Ben Norris <code@bsn.design>
2016-08-03 05:52:53 -0600

    Convert to Swift 3

--- Sources/VersionNumberService.swift
+++ Sources/VersionNumberService.swift
@@ -12,17 +12,17 @@ public struct VersionNumberService {
     // MARK: - Public properties
     
     public var versionNumber: String {
-        guard let shortVersion = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String else { return "Unknown" }
+        guard let shortVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else { return "Unknown" }
         return shortVersion
     }
     
     public var buildNumber: String {
-        guard let version = NSBundle.mainBundle().infoDictionary?["CFBundleVersion"] as? String else { return "Unknown" }
+        guard let version = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return "Unknown" }
         return version
     }
     
     public var appNameWithVersion: String {
-        guard let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? String else { return "Unnamed App" }
+        guard let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String else { return "Unnamed App" }
         return "\(appName) version \(versionNumber) (\(buildNumber))"
     }
     
